91de4e8ecb01616df35821a7b833c3cad7d64fb1
Yuriy Logvinov <yuriy.logvinov@adonis.no>
2021-03-11 00:21:44 +0200

    ++ steps

--- src/test/java/tests/AtomJenkins.java
+++ src/test/java/tests/AtomJenkins.java
@@ -3,10 +3,7 @@ package tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static java.lang.Boolean.TRUE;
-import static java.lang.Boolean.parseBoolean;
-import static java.lang.System.*;
-import static java.lang.System.getProperty;
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,43 +22,57 @@ public class AtomJenkins {
 
     @Test
     void failedTest() {
+
+        step("Hamcrest-ed false assertion");
         fail();
+    }
+
+
+    @Test
+    @DisplayName("Not Hamcrest-ed false assertion")
+    void failedTest1_5() {
+        step("Not Hamcrest-ed false assertion");
+        assertEquals(false, true);
     }
 
 
     @Test
     @DisplayName("Test from captain obviousness")
     void successTest3() {
-        assertEquals(true==true,false==false);
+        assertEquals(true == true, false == false);
     }
 
 
     @Test
     void successTest4() {
-       assertThat("true condition",true,is(true));
+        assertThat("true condition", true, is(true));
     }
 
     @Test
     void successTest5() {
-        assertThat(true,is(true));
+        assertThat(true, is(true));
     }
 
     @Test
+    @DisplayName("Hamcrest-ed PASSED test")
     void successTest6() {
+        step("Ham-assertion");
+        assertThat(true, is(true));
+    }
+
+    @Test
+    @DisplayName("NOT-Hamcrest-ed PASSED test")
+    void successTestNotHamcrested() {
+        step("NOT-Ham-assertion");
         assertTrue(true);
     }
 
     @Test
-    void successTest7() {
-        assertTrue(true);
-    }
-
-    @Test
+    @DisplayName("Hamcrest-ed Failed test")
     void failedTest2() {
-        assertThat("false condition",true, is(false));
+        step("Hamcrest-ed false assertion");
+        assertThat("false condition", true, is(false));
     }
-
-
 
 
 }
