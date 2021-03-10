7bafb46cfa97e6bea106775db5f2c2cc70ec649b
Yuriy Logvinov <yuriy.logvinov@adonis.no>
2021-03-11 00:12:18 +0200

    +hamcrest

--- src/test/java/tests/AtomJenkins.java
+++ src/test/java/tests/AtomJenkins.java
@@ -3,15 +3,18 @@ package tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.Boolean.TRUE;
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.System.*;
 import static java.lang.System.getProperty;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AtomJenkins {
 
     @Test
-    void successTest() {
+    void successTest1() {
         assertTrue(true);
     }
 
@@ -31,6 +34,34 @@ public class AtomJenkins {
     void successTest3() {
         assertEquals(true==true,false==false);
     }
+
+
+    @Test
+    void successTest4() {
+       assertThat("true condition",true,is(true));
+    }
+
+    @Test
+    void successTest5() {
+        assertThat(true,is(true));
+    }
+
+    @Test
+    void successTest6() {
+        assertTrue(true);
+    }
+
+    @Test
+    void successTest7() {
+        assertTrue(true);
+    }
+
+    @Test
+    void failedTest2() {
+        assertThat("false condition",true, is(false));
+    }
+
+
 
 
 }
